1b9dbf4cc5b09c90ec249a44b8e6eb8747846f90
jagdish6666 <jadhaojagdish63@gmail.com>
2026-01-01 09:14:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+export const arr = [10, 20, 30, 40]
+const array = [10, 20, 30, 40]  
+const user = 'jagdish'// or whatever array you want
+export default user
---
+++ script.js
@@ -0,0 +1,4 @@
+import array from './app.js'
+import {arr} from './app.js'
+import user from './app.js'
+console.log(arr,user)
